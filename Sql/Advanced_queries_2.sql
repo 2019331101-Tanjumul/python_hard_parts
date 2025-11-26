808c410cd9afbe7b1570b7917e4fb72c4338334e
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-11-27 02:34:54 +0600

    Task : add a new column with the 10% bonus based on each employees salary.

--- Sql/Advanced_queries_2.sql
+++ Sql/Advanced_queries_2.sql
@@ -1,7 +1,7 @@
 -- Drop the table if it exists
 DROP TABLE IF EXISTS employees;
 
--- Create the table
+-- Task
 CREATE TABLE employees(
     id SERIAL PRIMARY KEY,
     fname VARCHAR(50) NOT NULL,
@@ -22,19 +22,5 @@ INSERT INTO employees (fname, salary) VALUES
 ('Lsa Wilson', 7000.00),
 ('James Taylor', 54000.00);
 
-/*Here now i want to add an another table with set amount of words like the if else statemeny type now :*/
--- Display all records
-
--- select fname , salary , 
--- case when salary >= 30000 then 'Liveable' 
--- else 'Not liveable'
--- end as salary_category from employees;
-
-select fname,salary, case 
-when salary > 50000 then 'Liveable'
-when salary between 250000 and 35000 then 'Mid level'
-when salary <= 15000 then 'Misarable' 
-else 'semi liveable'
-end as salary_type from employees order by salary desc; 
-
-
+select fname , salary, case when salary > 0 then Round(salary*.10)
+end as Bonus from employees order by salary asc; 
