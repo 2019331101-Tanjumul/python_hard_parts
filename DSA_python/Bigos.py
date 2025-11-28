9bd20919081e310dc066cc0fc4114d4fc55494fd
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-11-28 22:15:40 +0600

    O(n)

--- DSA_python/Bigos.py
+++ DSA_python/Bigos.py
@@ -1,5 +1,18 @@
-#big O of O(1) , no matters how much the input it take the order / function here is simple
-def multily_num (n): 
-    return n*n
+#big O of O(1) , no matters how much the input it take the order of operations will / function here is simple and constant
 
-print(multily_num(2))
+#def multily_num (n): 
+#     return n*n
+
+# print(multily_num(2))
+
+
+
+
+#linear time complexity here O(N), the amount of time it takes the input the times it grows to the output 
+
+def lin_in(n): 
+    for i in range(n): 
+        print(i)
+
+lin_in(100)
+
